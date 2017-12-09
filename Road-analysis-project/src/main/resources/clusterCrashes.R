54a5bc5c1cd5718c4e3438ef0be240089d4b2342
111 Kobyshev <111 Kobyshev>
2017-12-10 00:17:21 +0300

    added reasons of clusters

--- Road-analysis-project/src/main/resources/clusterCrashes.R
+++ Road-analysis-project/src/main/resources/clusterCrashes.R
@@ -1,6 +1,6 @@
 library("rjson") 
 library(cluster)
-
+setwd("C:/Users/111/Desktop/Новая папка (3)/Road-analysis/Road-analysis-project/src/main/resources")
 jsonData <- fromJSON(file="2016-crash.json")
 jsonData <- jsonData$items
 
@@ -14,10 +14,13 @@ filterRegionsCondition <- sapply(jsonData, function(x) {
 filteredData<-jsonData[filterRegionsCondition]
 
 data<-matrix(unlist(filteredData),nrow=length(unlist(filteredData[[1]])))
+emTypeName<-data[1,]
 latitudes<-data[5,]
 longitudes<-data[6,]
 plot(longitudes,latitudes,col='black')
-
-coords<-data.frame(latitudes,longitudes)
-cl <- kmeans(coords,300, iter.max = 100)
+accidents<-data.frame(latitudes,longitudes,emTypeName)
+cl <- kmeans(data.frame(accidents$longitudes,accidents$latitudes),300, iter.max = 100)
 plot(longitudes,latitudes,col=cl$cluster)
+smallClusters<-with(cl,which(size<5,arr.ind = TRUE))
+reasons<-table(cl$cluster,emTypeName)
+reasons<-reasons[-smallClusters,]
