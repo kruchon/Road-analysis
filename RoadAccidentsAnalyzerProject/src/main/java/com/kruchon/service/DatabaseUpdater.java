19314503e71ba4e5197eea90eda625dcb83f02e1
111 Kobyshev <111 Kobyshev>
2017-12-31 00:24:05 +0300

    daemon for downloading and unpack accidents data finished

--- RoadAccidentsAnalyzerProject/src/main/java/com/kruchon/service/DatabaseUpdater.java
+++ RoadAccidentsAnalyzerProject/src/main/java/com/kruchon/service/DatabaseUpdater.java
@@ -1,7 +1,7 @@
 package com.kruchon.service;
 
 import com.kruchon.utils.FileOperationUtils;
-import org.apache.log4j.Logger;
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -9,13 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public class DatabaseUpdater {
 
-    final static Logger LOG = Logger.getRootLogger();
-
     private static ScheduledExecutorService updateDatabaseService = null;
+    private static String resourcesPath = null;
 
     public static void init(int hourDelay){
         if(updateDatabaseService != null){
             throw new RuntimeException("Repeated initialization");
+        }
+
+        //TODO Bad solution, it's better to map resources folder
+        resourcesPath = System.getProperty("user.dir").replace("bin","resources/");
+        File resourcesFolder = new File(resourcesPath);
+        if (!resourcesFolder.exists()) {
+            resourcesFolder.mkdir();
         }
 
         updateDatabaseService = Executors.newScheduledThreadPool(1);
@@ -27,8 +33,10 @@ public class DatabaseUpdater {
 
         @Override
         public void run() {
+
             try {
-                FileOperationUtils.downloadUsingNIO("https://xn--80abhddbmm5bieahtk5n.xn--p1ai/opendata-storage/2015-crash.json.zip","C://Users/111/Desktop/Новая папка (4)/first.zip");
+                FileOperationUtils.downloadUsingNIO("https://xn--80abhddbmm5bieahtk5n.xn--p1ai/opendata-storage/2015-crash.json.zip",resourcesPath+"first.zip");
+                FileOperationUtils.unpack(resourcesPath+"first.zip",resourcesPath);
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- RoadAccidentsAnalyzerProject/src/main/java/com/kruchon/utils/FileOperationUtils.java
+++ RoadAccidentsAnalyzerProject/src/main/java/com/kruchon/utils/FileOperationUtils.java
@@ -1,11 +1,13 @@
 package com.kruchon.utils;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class FileOperationUtils {
 
@@ -30,5 +32,30 @@ public class FileOperationUtils {
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         fos.close();
         rbc.close();
+    }
+
+    public static void unpack(String path, String dir_to) throws IOException {
+        ZipFile zip = new ZipFile(path);
+        Enumeration entries = zip.entries();
+        LinkedList<ZipEntry> zfiles = new LinkedList<ZipEntry>();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            if (entry.isDirectory()) {
+                new File(dir_to+"/"+entry.getName()).mkdir();
+            } else {
+                zfiles.add(entry);
+            }
+        }
+        for (ZipEntry entry : zfiles) {
+            InputStream in = zip.getInputStream(entry);
+            OutputStream out = new FileOutputStream(dir_to+"/"+entry.getName());
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = in.read(buffer)) >= 0)
+                out.write(buffer, 0, len);
+            in.close();
+            out.close();
+        }
+        zip.close();
     }
 }
