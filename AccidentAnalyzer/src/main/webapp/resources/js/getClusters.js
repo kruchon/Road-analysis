64754ddd0d43ec4bc19be5cdf3d57518b4cc005c
kruchon <krushon96@mail.ru>
2018-03-26 23:24:15 +0300

    added clusters markering on map

--- AccidentAnalyzer/src/main/webapp/resources/js/getClusters.js
+++ AccidentAnalyzer/src/main/webapp/resources/js/getClusters.js
@@ -1,4 +1,23 @@
 var clusters;
+var clusterMarkers = [];
+
+function drawMarkers() {
+    for(i=0; i<clusterMarkers.length; i++){
+        clusterMarkers[i].setMap(null);
+    }
+    clusterMarkers = [];
+    for(i=0;i<clusters.length;i++){
+        var latitude = clusters[i].latitude;
+        var longitude = clusters[i].longitude;
+        var latLng = new google.maps.LatLng(latitude,longitude);
+        var marker = new google.maps.Marker({
+            position:latLng
+        });
+        marker.setMap(map);
+        clusterMarkers.push(marker);
+    }
+}
+
 function getClusters() {
     var minSizeValue = document.getElementById('minSize').value;
     var minPercentValue = document.getElementById('minPercent').value;
@@ -7,5 +26,6 @@ function getClusters() {
         minPercent: minPercentValue
     }).done(function (response) {
         clusters = JSON.parse(response);
+        drawMarkers();
     });
 }
